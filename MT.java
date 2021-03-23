acc894ae0f1f8cd86d381923aa5d1a2c94fed59c
prathap <prathap@gmail.com>
2021-03-23 10:04:21 +0530

    first commit

---
+++ MT.java
@@ -0,0 +1,28 @@
+class Semaphoreextendsthread
+{
+ public void run()
+{
+for(int i=1;i<=5;i++)
+{
+system.out.println("Semaphore+i");
+}
+}
+public class MT
+{
+public static void main(String args[])
+{
+int s=1,i=2;
+for(i=1;i<=0;i++)
+{
+Semaphore=new Semaphore();
+while(S<=0);
+{
+S--;
+c1.Start();
+S++;
+}
+
+
+
+
+
